835c31052ca9b72d11e3568b5761b0e69bdddda5
unknown <6231718@qq.com>
2022-12-17 22:21:19 +0800

    First commit 2022-12-17 22:11:00

---
+++ test1.py
@@ -0,0 +1,18 @@
+print("abc")
+aa=11
+bb=22
+print(aa+bb)
+
+print("==============================================")
+
+print(
+    "Hello World!"
+    "Number is 198"
+    "http://c.biancheng.net/python/"
+);
+print("Hello World!" "Python is great!" "Number is 198.")
+print(
+    "Hello World!\n"
+    "Number is 198\n"
+    "http://c.biancheng.net/python/"
+);
